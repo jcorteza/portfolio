61596f2c344654deb3e8ef6736f2b2ebf6398090
JCortezAyala <40844882+jcorteza@users.noreply.github.com>
2019-02-14 13:18:13 -0600

    updated html routes and added static files middleware

---
+++ routes/html-routes.js
@@ -0,0 +1,27 @@
+module.export = (app) => {
+    // html route for homepage
+    app.get("/", (req, res) => {
+        console.log("getting homepage html route");
+        res.status(200).sendFile("../public/html/home.html");
+    });
+    //html route for about page
+    app.get("/about", (req, res) => {
+        console.log("getting about html route");
+        res.status(200).sendFile("../public/html/about.html");
+    });
+    //html route for contact page
+    app.get("/contact", (req, res) => {
+        console.log("getting contact page html route.");
+        res.status(200).sendFile("../public/html/contact.html");
+    });
+    //html route for portfolio page
+    app.get("/portfolio", (req, res) => {
+        console.log("getting portfolio page html route");
+        res.status(200).sendFile("../public/html/portfolio.html");
+    });
+    //html route for nonexistent pages
+    app.get("/*", (req, res) => {
+        console.log("getting 404 route");
+        res.status(404).sendFile("../public/html/notFound.html");
+    });
+}
---
+++ server.js
@@ -0,0 +1,11 @@
+const express = ("express");
+const app = express();
+let PORT = process.env.PORT || 3020;
+
+app.use(express.static("public"));
+
+require("./routes/html-routes")(app);
+
+app.listen(PORT, (req, res) => {
+    console.log(`App connected on http://localhost:${PORT}`);
+});
