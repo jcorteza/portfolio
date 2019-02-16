5b77e826343fda7c0d2f9e44eccf36ccb39cdde0
JCortezAyala <40844882+jcorteza@users.noreply.github.com>
2019-02-15 22:24:38 -0600

    modified css and html; troubleshooting css problem

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 let PORT = process.env.PORT || 3020;
 
-app.use(express.static("public"));
+//requiring dependencies
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public/"));
 
+//requiring routes
 require("./routes/html-routes")(app);
 
 app.listen(PORT, (req, res) => {
